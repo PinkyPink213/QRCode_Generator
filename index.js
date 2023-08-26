e26317f50fd04994c25d122353fce0d4d10e13d0
Pink <isariya@rs.co.th>
2023-08-27 02:19:14 +0800

    update script

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 let colorLight = "#ffffff",
     colorDark = "#000000",
-    text = "QR Code Text",
+    defaultText = "QR Code Text",
+    text = defaultText,
     size = 400;
 
 const getText = document.querySelector('#mytext')
 const qrcode  = document.querySelector('#qrcode')
+const download = document.querySelector(".download");
+const shareBtn = document.querySelector(".share");
+const currentSize = document.querySelector(".size");
+const light = document.querySelector("#color-one");
+const dark = document.querySelector("#color-two");
+
+
+currentSize.addEventListener("change", handleSize);
 getText.addEventListener("input", handleText);
+dark.addEventListener("input", handleDarkColor);
+light.addEventListener("input", handleLightColor);
+// shareBtn.addEventListener("click", handleShare);
+
 // getText.innerHTML = text;
 getText.placeholder = text;
 function handleText(e){
     const value = e.target.value;
     text = value;
-    console.log(text)
+    if (!text) {
+        text = defaultText;
+    }
     generateQRCode()
 }
+function handleSize(e) {
+    size = e.target.value;
+    generateQRCode();
+}
+function handleDarkColor(e) {
+    colorDark = e.target.value;
+    generateQRCode();
+}
 
+function handleLightColor(e) {
+    colorLight = e.target.value;
+    generateQRCode();
+}
 function generateQRCode() {
     qrcode.innerHTML = "";
+    console.log(size)
     new QRCode(document.getElementById("qrcode"), {
         text: text,
         width: size,
@@ -27,11 +55,3 @@ function generateQRCode() {
     });
 }
 generateQRCode()
-    // new QRCode("qr-code", {
-    //     text,
-    //     height: size,
-    //     width: size,
-    //     colorLight,
-    //     colorDark,
-    // });
-    // download.href = await resolveDataUrl();
